47575dd0395c87a919afe5cfc45a719390449c82
Álvaro Sendón Aleu <alvaro.sendon.aleu@gmail.com>
2026-02-07 16:28:25 +0100

    Fix JSX runtime: use Preact h/Fragment via Vite esbuild

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+    jsxInject: `import { h, Fragment } from 'preact'`,
+  },
   resolve: {
     alias: {
       'react': 'preact/compat',
